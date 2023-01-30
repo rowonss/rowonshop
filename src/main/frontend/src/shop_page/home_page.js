fabf08a319406827c04f302ba8135f56d764047d
rowonss <95suskjg@naver.com>
2023-01-30 11:30:20 +0900

    2023 01 30

--- src/main/frontend/src/shop_page/home_page.js
+++ src/main/frontend/src/shop_page/home_page.js
@@ -3,7 +3,6 @@ import mainphoto from "../images/main.png";
 const home_page = () => {
     return (
         <div className="home_page">
-            <p>ㅎㅇㅎㅇ</p>
             <img alt="main" src={mainphoto}/>
         </div>
     )
